946e3f058d9c639911cad439135b923e1f742960
sneaky.ruds <sneaky.ruds@dc434f98-ee47-0410-b896-675d497ead5a>
2013-09-15 11:12:17 +0000

    Update for the new log file format.

--- combat_logs/log_parser.py
+++ combat_logs/log_parser.py
@@ -28,16 +28,16 @@ import sys
 
 class UTC(datetime.tzinfo):
     """UTC"""
-    __ZERO = datetime.timedelta(0)
+    _ZERO = datetime.timedelta(0)
 
     def utcoffset(self, dt):
-        return self.__ZERO
+        return self._ZERO
 
     def tzname(self, dt):
         return "UTC"
 
     def dst(self, dt):
-        return self.__ZERO
+        return self._ZERO
 
 
 _TIMESTAMP_PATTERN = (
@@ -73,7 +73,7 @@ class LogEntry(object):
     def data(self):
         return self._data
 
-    __LOG_LINE_RE = re.compile(
+    _LOG_LINE_RE = re.compile(
         r'^\[ %s \] \((?P<type>[^)]+)\) (?P<data>.+)$'
         % _TIMESTAMP_PATTERN)
 
@@ -86,7 +86,7 @@ class LogEntry(object):
         message and is usually because multiple lines of text were shown
         to a user.
         """
-        m = cls.__LOG_LINE_RE.match(line)
+        m = cls._LOG_LINE_RE.match(line)
         if m is None:
             return None
         entry_type = m.group('type')
@@ -140,19 +140,25 @@ class CombatLogEntry(LogEntry):
         return self._damage
 
     def _parse_data(self, log):
-        if log.log_type == Log.COMPLEX:
+        if log.log_type == Log.V3:
+            self._parse_v3()
+        elif log.log_type == Log.COMPLEX:
             self._parse_complex()
         elif log.log_type == Log.SIMPLIFIED:
             self._parse_simple()
         else:
             try:
-                self._parse_simple()
-                log.log_type = Log.SIMPLIFIED
+                self._parse_v3()
+                log.log_type = Log.V3
             except ValueError:
-                self._parse_complex()
-                log.log_type = Log.COMPLEX
+                try:
+                    self._parse_simple()
+                    log.log_type = Log.SIMPLIFIED
+                except ValueError:
+                    self._parse_complex()
+                    log.log_type = Log.COMPLEX
 
-    __VERB_PHRASES = [
+    _VERB_PHRASES = [
         '%(attacker)s (?:lightly |heavily )?hits %(target)s, %(damage)s\.$',
         '%(attacker)s misses %(target)s completely\.(?!%(damage)s)$',
         '%(attacker)s aims well at %(target)s, %(damage)s\.$',
@@ -167,28 +173,29 @@ class CombatLogEntry(LogEntry):
         '%(attacker)s perfectly strikes %(target)s, %(damage)s\.$',
         ]
 
-    __ATTACKER_PATTERNS = [ '(?P<attacker>You)r (?:group of )?(?P<weapon>.*?)',
+    _ATTACKER_PATTERNS = [ '(?P<attacker>You)r (?:group of )?(?P<weapon>.*?)',
                             '(?P<weapon>.*?) belonging to (?P<attacker>.*?)',
                             '(?P<attacker>.*?)(?P<weapon>)' ]
 
-    __DAMAGE_PATTERN = r'[^,]*?(?P<damage>\d+\.\d+)?(?:</b>)? damage'
+    _DAMAGE_PATTERN = r'[^,]*?(?P<damage>\d+\.\d+)?(?:</b>)? damage'
 
-    __VERB_PHRASE_RES = [
+    _VERB_PHRASE_RES = [
         re.compile(vp % { 'attacker': '(?:<color[^>]*>)?%s' % a,
                           'target': '(?P<target>.*?)',
-                          'damage': __DAMAGE_PATTERN })
-        for vp in __VERB_PHRASES
-        for a in __ATTACKER_PATTERNS
+                          'damage': _DAMAGE_PATTERN })
+        for vp in _VERB_PHRASES
+        for a in _ATTACKER_PATTERNS
         ]
 
     def _parse_complex(self):
         m = None
-        for rex in self.__VERB_PHRASE_RES:
+        for rex in self._VERB_PHRASE_RES:
             m = rex.match(self._data)
             if m is not None:
                 break
         if m is None:
-            raise ValueError('Could not parse """%s""".' % self._data)
+            raise ValueError(
+                'Could not parse """%s""" as complex.' % self._data)
 
         self._target = m.group('target')
         self._attacker = m.group('attacker')
@@ -199,7 +206,7 @@ class CombatLogEntry(LogEntry):
         else:
             self._damage = float(damage)
 
-    __SIMPLIFIED_PHRASES = [
+    _SIMPLIFIED_PHRASES = [
         ('(?:<color[^>]*>)?(?P<attacker>.*) (?:hits|strikes) (?P<target>you) '
          'for %(simple_damage)s$'),
         ('(?:<color[^>]*>)?(?P<weapon>.*) (?:hits|strikes) (?P<target>.*) '
@@ -210,21 +217,21 @@ class CombatLogEntry(LogEntry):
         '(?:<color[^>]*>)?(?P<weapon>.*) miss (?P<target>[^.]*)(?P<damage>)$',
         ]
 
-    __SIMPLIFIED_PHRASE_RES = [
+    _SIMPLIFIED_PHRASE_RES = [
         re.compile(sp % {
                 'simple_damage': ('<b>(?P<damage>\d+)</b> damage'
                                   '(?: \(Wrecking!\))?') })
-        for sp in __SIMPLIFIED_PHRASES
+        for sp in _SIMPLIFIED_PHRASES
         ]
 
     def _parse_simple(self):
         m = None
-        for rex in self.__SIMPLIFIED_PHRASE_RES:
+        for rex in self._SIMPLIFIED_PHRASE_RES:
             m = rex.match(self._data)
             if m is not None:
                 break
         if m is None:
-            raise ValueError('Could not parse """%s""".' % self._data)
+            raise ValueError('Could not parse """%s""" as simple.' % self._data)
 
         self._target = m.group('target')
         if self._target == 'you':
@@ -239,12 +246,65 @@ class CombatLogEntry(LogEntry):
         else:
             self._damage = 0
 
+    _V3_PHRASES = [
+        ('<color[^>]*><b>(?P<damage>[0-9]+)'
+         '</b> <color[^>]*><font[^>]*>(?P<preposition>.*)</font> '
+         '<b><color[^>]*>(?P<object>.*)'
+         '</b><font[^>]*><color[^>]*>(?: - (?P<weapon>.*))?'
+         ' - (?:Glances Off|Grazes|Hits|Penetrates|Smashes|Wrecks)$'),
+        ('(?P<attacker>You)r (?P<weapon>.*) misses (?P<target>.*) completely'
+         r' - \2$'),
+        '(?P<attacker>.*) misses (?P<target>you) completely$',
+        ('<color[^>]*><b>(?P<ew>.*)</b> <color[^>]*><font[^>]*>from</font> '
+         '<color[^>]*><b>(?P<attacker>.*)</b> <color[^>]*><font[^>]*>to '
+         '<b><color[^>]*></font>(?P<target>.*)!$'),
+        ]
+
+    _V3_PHRASE_RES = [re.compile(phrase) for phrase in _V3_PHRASES]
+
+    def _parse_v3(self):
+        m = None
+        for rex in self._V3_PHRASE_RES:
+            m = rex.match(self._data)
+            if m is not None:
+                break
+        if m is None:
+            raise ValueError('Could not parse """%s""" as V3' % self._data)
+
+        d = m.groupdict()
+        if 'preposition' in d:
+            p = d['preposition']
+            assert p == 'to' or p == 'from'
+            if p == 'to':
+                self._target = d['object']
+                self._attacker = 'You'
+            else:
+                self._target = 'you'
+                self._attacker = d['object']
+        else:
+            self._target = d['target']
+            if self._target == 'you':
+                self._attacker = d['attacker']
+            else:
+                self._attacker = 'You'
+
+        if self._target == 'you':
+            self._weapon = ''
+        else:
+            self._weapon = d['weapon']
+
+        if 'damage' in d:
+            self._damage = int(d['damage'])
+        else:
+            self._damage = 0
+
 
 class Log(object):
     # Log types:
     UNKNOWN = 0
     COMPLEX = 1
     SIMPLIFIED = 2
+    V3 = 3
 
     """A log consists of some metadata and a sequence of log entries."""
     def __init__(self, listener, start_time, infile):
@@ -294,40 +354,40 @@ class Log(object):
             infile = log_file
 
         try:
-            listener, timestamp = cls.__read_header(infile)
+            listener, timestamp = cls._read_header(infile)
             return Log(listener, timestamp, infile)
         finally:
             infile.close()
 
-    __MINUSES_RE = re.compile('^-+$')
-    __GAMELOG_RE = re.compile('Gamelog')
-    __LISTENER_RE = re.compile('Listener: (.*)')
-    __SESSION_RE = re.compile(
-        'Session started: %s' % _TIMESTAMP_PATTERN)
+    _MINUSES_RE = re.compile('^-+$')
+    _GAMELOG_RE = re.compile('Gamelog')
+    _LISTENER_RE = re.compile('Listener: (.*)')
+    _SESSION_RE = re.compile(
+        'Session [Ss]tarted: %s' % _TIMESTAMP_PATTERN)
 
     @classmethod
-    def __read_header(cls, infile):
+    def _read_header(cls, infile):
         try:
-            if not cls.__MINUSES_RE.match(infile.next().rstrip()):
+            if not cls._MINUSES_RE.match(infile.next().rstrip()):
                 raise ValueError('Missing --- line at start of file.')
-            if not cls.__GAMELOG_RE.search(infile.next().rstrip()):
+            if not cls._GAMELOG_RE.search(infile.next().rstrip()):
                 raise ValueError('Missing "Gamelog" line in header.')
             # Empty logs don't have listeners...
             maybe_listener_line = infile.next().rstrip()
-            m = cls.__LISTENER_RE.search(maybe_listener_line)
+            m = cls._LISTENER_RE.search(maybe_listener_line)
             if m is None:
                 listener = None
                 session_start_line = maybe_listener_line
             else:
                 listener = m.group(1)
                 session_start_line = infile.next().rstrip()
-            m = cls.__SESSION_RE.search(session_start_line)
+            m = cls._SESSION_RE.search(session_start_line)
             if m is None:
                 raise ValueError('Missing "Session started" line in header.')
             y, mo, d, h, mi, s = map(int, m.group('year', 'month', 'day',
                                                   'hour', 'min', 'sec'))
             timestamp = datetime.datetime(y, mo, d, h, mi, s, tzinfo = UTC())
-            if not cls.__MINUSES_RE.match(infile.next().rstrip()):
+            if not cls._MINUSES_RE.match(infile.next().rstrip()):
                 raise ValueError('Missing --- line to end header.')
 
         except StopIteration:
